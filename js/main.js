c252623aeff41a4ab2a46464e337add8fe56b3b2
Ali-CP <110411510+alim-99@users.noreply.github.com>
2023-08-03 16:48:43 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+new Date().getFullYear()
+let date = new Date();
+console. log(date. getFullYear());
+const copyrightFooter = ` <span> Copyright © ${new Date(). getFullYear()} <div class="rights">All Rights Reseved</div></span> `;
+document. getElementById('year'). innerHTML = copyrightFooter;
